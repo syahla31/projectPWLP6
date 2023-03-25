e4148344d8a3f106065f4b5ef7705c6f57c7fcda
syahla31 <syahlasyafiqah31@gmail.com>
2023-03-25 11:51:19 +0700

    JS 6 - Pratikum 3

---
+++ database/seeders/NewUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class NewUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'name' => 'Administator Baru',
+            'email' => 'admin.baru@admin.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
